88c7acf4a6141e627db8a07894da846dd7247aa9
Alison Fernando <alisonteobaldo2001@gmail.com>
2023-03-20 17:40:59 -0300

    Inicando configuração do menu

--- UI/Form1.Designer.cs
+++ UI/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cadastrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatóriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colaboradoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.equipamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoColaboradoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoEquipamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarToolStripMenuItem,
+            this.visualizarToolStripMenuItem,
+            this.relatóriosToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // cadastrarToolStripMenuItem
+            // 
+            this.cadastrarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colaboradoresToolStripMenuItem,
+            this.equipamentosToolStripMenuItem});
+            this.cadastrarToolStripMenuItem.Name = "cadastrarToolStripMenuItem";
+            this.cadastrarToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.cadastrarToolStripMenuItem.Text = "Cadastrar";
+            // 
+            // visualizarToolStripMenuItem
+            // 
+            this.visualizarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoColaboradoresToolStripMenuItem,
+            this.infoEquipamentosToolStripMenuItem});
+            this.visualizarToolStripMenuItem.Name = "visualizarToolStripMenuItem";
+            this.visualizarToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.visualizarToolStripMenuItem.Text = "Visualizar";
+            // 
+            // relatóriosToolStripMenuItem
+            // 
+            this.relatóriosToolStripMenuItem.Name = "relatóriosToolStripMenuItem";
+            this.relatóriosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.relatóriosToolStripMenuItem.Text = "Relatórios";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // colaboradoresToolStripMenuItem
+            // 
+            this.colaboradoresToolStripMenuItem.Name = "colaboradoresToolStripMenuItem";
+            this.colaboradoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.colaboradoresToolStripMenuItem.Text = "Colaboradores";
+            // 
+            // equipamentosToolStripMenuItem
+            // 
+            this.equipamentosToolStripMenuItem.Name = "equipamentosToolStripMenuItem";
+            this.equipamentosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.equipamentosToolStripMenuItem.Text = "Equipamentos";
+            // 
+            // infoColaboradoresToolStripMenuItem
+            // 
+            this.infoColaboradoresToolStripMenuItem.Name = "infoColaboradoresToolStripMenuItem";
+            this.infoColaboradoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.infoColaboradoresToolStripMenuItem.Text = "Info Colaboradores";
+            // 
+            // infoEquipamentosToolStripMenuItem
+            // 
+            this.infoEquipamentosToolStripMenuItem.Name = "infoEquipamentosToolStripMenuItem";
+            this.infoEquipamentosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.infoEquipamentosToolStripMenuItem.Text = "Info Equipamentos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem cadastrarToolStripMenuItem;
+        private ToolStripMenuItem colaboradoresToolStripMenuItem;
+        private ToolStripMenuItem equipamentosToolStripMenuItem;
+        private ToolStripMenuItem visualizarToolStripMenuItem;
+        private ToolStripMenuItem infoColaboradoresToolStripMenuItem;
+        private ToolStripMenuItem infoEquipamentosToolStripMenuItem;
+        private ToolStripMenuItem relatóriosToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
     }
 }
--- UI/Form1.cs
+++ UI/Form1.cs
@@ -6,5 +6,10 @@ namespace UI
         {
             InitializeComponent();
         }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
     }
 }
